b80f9850aa052f7d7ca56a57cfab9215cca1e042
Arya <aryatiwari3101@gmail.com>
2024-08-18 18:47:29 +0530

    upload

---
+++ medicoplus/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-white text-3xl underline">Home</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+    
+          
+            
+}
